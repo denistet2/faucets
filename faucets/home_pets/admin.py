cfd0d06ea5e8b243099b7402b731165ed8b936e0
dzianistsetserukou <denistet@gmail.com>
2023-01-17 00:02:46 +0300

    добавил модели в админку

--- faucets/home_pets/admin.py
+++ faucets/home_pets/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
-from .models import HomePet, OrderPet, OrderPetTemporarity, TransportationOrder, VolOrder
+from .models import *
+
+class HomePetAdmin(admin.ModelAdmin):
+    list_display = ('pets','foto','name_pets','age_pets','text_about_pets')
+admin.site.register(HomePet,HomePetAdmin)
+class OrderPetAdmin(admin.ModelAdmin):
+    list_display = ('name','phone','pets','message','order_data')
+admin.site.register(OrderPet,OrderPetAdmin)
+
+class OrderPetTemporarityAdmin(admin.ModelAdmin):
+    list_display = ('name','phone','pets','order_data','start_time','end_time','message')
+admin.site.register(OrderPetTemporarity,OrderPetTemporarityAdmin)
 
 
-admin.site.register(HomePet)
-admin.site.register(OrderPet)
-admin.site.register(OrderPetTemporarity)
-admin.site.register(TransportationOrder)
-admin.site.register(VolOrder)
+class TransportationOrderAdmin(admin.ModelAdmin):
+    list_display = ('driver_name','phone','transport_type','order_data','start_point','end_point','message')
+admin.site.register(TransportationOrder,TransportationOrderAdmin)
+
+
+class VolOrderAdmin(admin.ModelAdmin):
+    list_display = ('name_vol','phone','vol_type','data_order','start_time','end_time','message')
+admin.site.register(VolOrder,VolOrderAdmin)
+
+admin.site.register(MedicamentosAdd)
+admin.site.register(ForageAdd)
+admin.site.register(ItemAdd)
--- faucets/home_pets/models.py
+++ faucets/home_pets/models.py
@@ -18,7 +18,7 @@ class HomePet(models.Model):
     foto = models.ImageField(upload_to='images', blank=True, verbose_name='Фото')
     name_pets = models.CharField('Кличка', max_length=20, help_text="Кличка")
     age_pets = models.IntegerField('Возвраст', help_text="Возраст")
-    text_about_pets = models.TextField(max_length=200, help_text="Замечания")
+    text_about_pets = models.TextField('Кратко о питомце',max_length=200, help_text="Замечания")
 
     # def __str__(self):
     #     return self.pets, self.name_pets, self.age_pets, self.foto, self.text_about_pets
@@ -29,11 +29,11 @@ class HelpList(models.Model):
 
 
 class OrderHelp(models.Model):
-    name = models.CharField(max_length=20, help_text="Имя")
+    name = models.CharField('Волонтер',max_length=20, help_text="Имя")
     help_type = models.CharField(max_length=10)
     email = models.EmailField()
     phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
+    phone = models.CharField('Телефон', validators=[phoneNumberRegex], max_length=16, unique=True)
 
 
 
@@ -44,25 +44,25 @@ class News(models.Model):
 
 
 class OrderPet(models.Model):
-    name = models.CharField(max_length=30, help_text="Фамилия и имя")
+    name = models.CharField('Заводчик', max_length=30, help_text="Фамилия и имя")
     # phone = models.CharField(max_length=10, help_text="Номер телефона")
     phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
-    pets = models.CharField(max_length=20, help_text="Питомец")
-    message = models.TextField(max_length=100, help_text="Замечания")
-    data_order_pet = models.DateTimeField(verbose_name="Дата")
+    phone = models.CharField('Телефон',validators=[phoneNumberRegex], max_length=16, unique=True)
+    pets = models.CharField('Кличка',max_length=20, help_text="Питомец")
+    message = models.TextField('Замечания',max_length=100, help_text="Замечания")
+    order_data = models.DateTimeField(verbose_name="Дата")
 
 
 class OrderPetTemporarity(models.Model):
-    name = models.CharField(max_length=30, help_text="Фамилия и имя")
+    name = models.CharField('Волонтер',max_length=30, help_text="Фамилия и имя")
     # phone = models.CharField(max_length=10, help_text="Номер телефона")
     phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
-    pets = models.CharField(max_length=20, help_text="Питомец")
-    data_order_pet = models.DateTimeField(verbose_name="Дата")
+    phone = models.CharField('Телефон',validators=[phoneNumberRegex], max_length=16, unique=True)
+    pets = models.CharField('Кличка',max_length=20, help_text="Питомец")
+    order_data = models.DateTimeField(verbose_name="Дата")
     start_time = models.DateTimeField(verbose_name="Начало")
     end_time = models.DateTimeField(verbose_name="Окончание")
-    message = models.TextField(max_length=100, help_text="Замечания")
+    message = models.TextField('Замечания',max_length=100, help_text="Замечания")
 
 
 class TransportationOrder(models.Model):
@@ -74,15 +74,15 @@ class TransportationOrder(models.Model):
      (4, 'Транспортировка больших собак')
 
     ]
-    driver_name = models.CharField(max_length=30, help_text="Инициалы")
+    driver_name = models.CharField('Перевозчик', max_length=30, help_text="Инициалы")
     # phone_numb = models.CharField(max_length=10, help_text="Телефон")
     phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
+    phone = models.CharField('Телефон', validators=[phoneNumberRegex], max_length=16, unique=True)
     transport_type = models.PositiveSmallIntegerField(null=True, choices=TRANSPORT_VAR, editable=False)
-    data_order_pet = models.DateTimeField(verbose_name="Дата")
-    start_time = models.DateTimeField(verbose_name="Начало")
-    end_tame = models.DateTimeField(verbose_name="Окончание")
-    message = models.TextField(max_length=100, help_text="Замечания")
+    order_data = models.DateTimeField(verbose_name="Дата")
+    start_point = models.CharField(max_length=30,verbose_name="Погрузить")
+    end_point = models.CharField(max_length=30,verbose_name="Выгрузить")
+    message = models.TextField('Замечания', max_length=100, help_text="Замечания")
 
 
 class VolOrder(models.Model):
@@ -91,12 +91,66 @@ class VolOrder(models.Model):
      (1, 'Хочу помогать на постоянной основе(расписание можно подобрать так, как будет вам удобно'),
      (2, 'Готов помогать иногда.Хочу выбрать удобное для меня время')
     ]
-    name_vol = models.CharField(max_length=30, help_text="Фамилия и имя")
+    name_vol = models.CharField('Волонтер', max_length=30, help_text="Фамилия и имя")
     # phone = models.CharField(max_length=10, help_text="Номер телефона")
     phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
     phone = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
     vol_type = models.PositiveSmallIntegerField(null=True, verbose_name='vol_type', editable=False, choices=VOL_VAR)
-    data_order_pet = models.DateTimeField(verbose_name="Дата")
+    data_order = models.DateTimeField(verbose_name="Дата")
     start_time = models.DateTimeField(verbose_name="Начало")
     end_time = models.DateTimeField(verbose_name="Окончание")
     message = models.CharField(max_length=100, help_text="Замечания")
+
+class MedicamentosAdd(models.Model):
+
+    MEDICAMENTOS_VAR = [
+        (1, 'цефтриаксон'),
+        (2, 'новокаин'),
+        (3, 'гискан'),
+        (4, 'гамавит'),
+        (5, 'катозал'),
+        (6, 'антитокс'),
+        (7, 'фоспренил'),
+        (8, 'энтеросгель'),
+        (9, 'препараты от глистов'),
+        (10, 'препараты от блох,клещей'),
+        (11, 'для заживления ран')
+    ]
+    med_type = models.PositiveSmallIntegerField(null=True, verbose_name='vol_type', editable=False, choices=MEDICAMENTOS_VAR)
+    med_weight = models.IntegerField()
+    name = models.CharField('Волонтер',max_length=30, help_text="Фамилия и имя")
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phone = models.CharField('Телефон', validators=[phoneNumberRegex], max_length=16, unique=True)
+
+
+class ForageAdd(models.Model):
+
+    FORAGE_VAR = [
+        (1, 'крупы'),
+        (2, 'Сухие корма'),
+        (3, 'Влажные корма'),
+        (4, 'Фарш для собак')
+
+    ]
+    forage_type = models.PositiveSmallIntegerField(null=True, verbose_name='vol_type', editable=False, choices=FORAGE_VAR)
+    forage_weight = models.IntegerField()
+    name = models.CharField('Волонтер',max_length=30, help_text="Фамилия и имя")
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phone = models.CharField('Телефон', validators=[phoneNumberRegex], max_length=16, unique=True)
+
+class ItemAdd(models.Model):
+
+    ITEM_VAR = [
+        (1, 'Ошейники, поводки, намордники'),
+        (2, 'Опилки и солома'),
+        (3, 'Посуда'),
+        (4, 'Теплые вещи'),
+        (5, 'Будки')
+
+    ]
+    item_type = models.PositiveSmallIntegerField(null=True, verbose_name='vol_type', editable=False, choices=ITEM_VAR)
+    item_qty = models.IntegerField()
+    which_item = models.CharField('Вещь', max_length=100, help_text="Замечания")
+    name = models.CharField('Волонтер',max_length=30, help_text="Фамилия и имя")
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phone = models.CharField('Телефон', validators=[phoneNumberRegex], max_length=16, unique=True)
